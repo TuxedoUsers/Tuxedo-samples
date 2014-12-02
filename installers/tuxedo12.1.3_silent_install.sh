f535b56e469fc6b0098f8b98ec02fecea9a37150
toddinpal <toddjlittle@gmail.com>
2014-12-02 14:55:19 -0600

    Updated installer script to apply rolling patch as well.

--- installers/tuxedo12.1.3_silent_install.sh
+++ installers/tuxedo12.1.3_silent_install.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 #
-# This procedure assumes that the Tuxedo installation kit and silent response file have been
-# downloaded to ~/Downloads.
+# This procedure assumes that the following files have been downloaded to ~/Downloads:
+#	tuxedo12.1.3_silent_install.sh		- This file
+#	tuxedo12.1.3.rsp			- Silent install response file 
+#	tuxedo121300_64_Linux_01_x86.zip	- Tuxedo installation kit from Oracle
+#	p19927652_121300_Linux-x86-64.zip	- Tuxedo lastest patch kit from Oracle - RP011
+#
+# Usage:  sh tuxedo12.1.3_silent_install.sh [patch-filename [tuxedo-installer-filename]]
 #
 # Created by Todd Little  1-Dec-2014
 #
+# Get the arguments
+INSTALLER=tuxedo121300_64_Linux_01_x86.zip
+PATCH=p19927652_121300_Linux-x86-64.zip
+if [ ! -z "$1" ]
+    then
+	PATCH=$1
+	if [ ! -z "$2" ]
+	    then
+		INSTALLER=$2
+	fi
+fi
+echo "Using patch file $PATCH"
+echo "Using Tuxedo installer $INSTALLER"
 # Unzip the downloaded installation kit
-unzip -qq tuxedo121300_64_Linux_01_x86.zip
+unzip -qq $INSTALLER
 # Run the installer in silent mode
 #
 # Installs all of Tuxedo including samples without LDAP support.  tlisten password is "oracle"
@@ -16,8 +34,17 @@ unzip -qq tuxedo121300_64_Linux_01_x86.zip
 #
 # Remove the installer, but not the kit or response file
 rm -Rf Disk1
-echo "Installation done"
-
-
+echo "Installation done, starting rolling patch"
+#
+# Install rolling patch
+export ORACLE_HOME=~/tuxhome
+unzip -qq $PATCH
+# Get the patch zip file name from the patch kit name, get everything before the first _
+zipname=`echo "$PATCH" | cut -d'_' -f1`
+# Strip of the p at the beginning
+zipname=${zipname:1:20}
+$ORACLE_HOME/OPatch/opatch apply $zipname.zip
+# Clean up patch files
+rm -f $zipname.zip patchlev README.txt releasenotes.txt
 
 
