f04e0695397cd6634e0540f99ef129d96aeb10ff
toddjlittle@gmail.com <tuxtest@tuxtest.localdomain>
2015-02-09 21:23:22 +0000

    Update docker files to use the new template mechanism for customizing the location of files and other OUI information.

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+set -x
 echo "====================="
+#
 
 if [ ! -e tuxedo121300_64_Linux_01_x86.zip ]
 then
@@ -19,13 +20,36 @@ then
   exit
 fi
 
+
+if [ ! -e p19927652_121300_Linux-x86-64.zip ]
+then
+  echo "Download the latest Tuxedo rolling patch kit and"
+  echo "drop the file p19927652_121300_Linux-x86-64.zip in this folder before"
+  echo "building this Tuxedo Docker container!"
+  exit 
+fi
+
+MD5="3b311c87e921fa9df696bf74c39c3348  p19927652_121300_Linux-x86-64.zip"
+MD5_CHECK="`md5sum p19927652_121300_Linux-x86-64.zip`"
+
+if [ "$MD5" != "$MD5_CHECK" ]
+then
+  echo "MD5 does not match! Download again!"
+  exit
+fi
+
 echo "====================="
+
+# Fix up the locations of things
+sh fix_locations.sh tuxedo.docker tuxedo12.1.3.rsp.template >tuxedo12.1.3.rsp
+sh fix_locations.sh tuxedo.docker tuxedo12.1.3_silent_install.sh.template >tuxedo12.1.3_silent_install.sh
 
 docker build -t oracle/tuxedo .
 if [ "$?" = "0" ]
     then
 	echo ""
-	echo "Tuxedo Docker image is ready to be used. To start, run 'dockTuxedo.sh'"
+	echo "Tuxedo Docker image is ready to be used. To create a container, run:"
+	echo "sudo docker run -i -t oracle/tuxedo /bin/bash"
     else
 	echo "Build of Tuxedo Docker image failed."
 fi
