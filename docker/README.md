88ac5b446af40ee867ea2e6432c856f5b8525fce
toddinpal <toddjlittle@gmail.com>
2015-11-14 21:39:02 -0600

    Cleaned up install/patch, made patching optional, rewrote README to match reality

--- docker/README.md
+++ docker/README.md
@@ -2,12 +2,23 @@
 This folder contains the information and examples of how to use [Tuxedo](http://oracle.com/tuxedo) with [Docker](https://www.docker.com/).
 
 ## Contents
-It is based on the WebLogic Server dockerization (is that even a word?) done by Bruno Borges.  Roughly the files are:
+It is based on the WebLogic Server dockerization (is that even a word?) done by Bruno Borges.
 
-1. install_tuxedo12.1.3.rsp - This is a silent install answer file for Tuxedo 12.1.3.  It allows Tuxedo to be installed without any user interaction.
-2. Dockerfile - The dockerfile used to take a base Oracle Linux 6.5 image and add Tuxedo to it
-3. dockTuxedo.sh - Script to run the docker Tuxedo image
-4. build.sh - Script to create the docker Tuxedo image
- 
+## To use
+1. Into an empty directory:
+  1. Download the Tuxedo 12.1.3 Linux 64 bit installer from OTN
+  2. Download tuxedo_docker.zip from this github directory
+  3. Optionally download the latest Tuxedo rolling patch from My Oracle Support
+2. Unzip tuxedo_docker.zip
+3. Execute build.sh
+
+You should end up with a docker image tagged oracle/tuxedo
+
+You can then start the image in a new container with:  `docker run -i -t oracle/tuxedo /bin/bash`
+which will put you into the container with a bash prompt.  If you want to test the new container, simply execute the `simpapp_runme.sh` in an empty
+directory and the script will build and run the Tuxedo simpapp application.
+
+Have fun!
+
 
 
--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 echo "====================="
 #
 
@@ -21,35 +21,33 @@ then
 fi
 
 
-if [ ! -e p19927652_121300_Linux-x86-64.zip ]
+if [ ! -e p*_121300_Linux-x86-64.zip ]
 then
-  echo "Download the latest Tuxedo rolling patch kit and"
-  echo "drop the file p19927652_121300_Linux-x86-64.zip in this folder before"
-  echo "building this Tuxedo Docker container!"
-  exit 
+  echo "Installing Tuxedo without any patches"
 fi
 
-MD5="3b311c87e921fa9df696bf74c39c3348  p19927652_121300_Linux-x86-64.zip"
-MD5_CHECK="`md5sum p19927652_121300_Linux-x86-64.zip`"
-
-if [ "$MD5" != "$MD5_CHECK" ]
-then
-  echo "MD5 does not match! Download again!"
-  exit
-fi
+#MD5="3b311c87e921fa9df696bf74c39c3348  p19927652_121300_Linux-x86-64.zip"
+#MD5_CHECK="`md5sum p19927652_121300_Linux-x86-64.zip`"
+#
+#if [ "$MD5" != "$MD5_CHECK" ]
+#then
+#  echo "MD5 does not match! Download again!"
+#  exit
+#fi
 
 echo "====================="
 
 # Fix up the locations of things
 sh fix_locations.sh tuxedo.docker tuxedo12.1.3.rsp.template >tuxedo12.1.3.rsp
 sh fix_locations.sh tuxedo.docker tuxedo12.1.3_silent_install.sh.template >tuxedo12.1.3_silent_install.sh
+sh fix_locations.sh tuxedo.docker Dockerfile.template >Dockerfile
 
 docker build -t oracle/tuxedo .
 if [ "$?" = "0" ]
     then
 	echo ""
 	echo "Tuxedo Docker image is ready to be used. To create a container, run:"
-	echo "sudo docker run -i -t oracle/tuxedo /bin/bash"
+	echo "docker run -i -t oracle/tuxedo /bin/bash"
     else
 	echo "Build of Tuxedo Docker image failed."
 fi
